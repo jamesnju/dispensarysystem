cc67d47b245cd94742499f86788b70df340c5ee2
jamesnju <james500nju@gmail.com>
2023-07-11 16:14:37 +0300

    updates on dispaying student details

--- dispensary/urls.py
+++ dispensary/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import dispensary_list
+from .views import dispensary_list,per_student_view
 
 urlpatterns = [
     path("", dispensary_list, name="dispensary_list"),
-    
+    path("details/<int:pk>", per_student_view, name="per_student_view"),   
 ]
--- dispensary/views.py
+++ dispensary/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Student
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
+
 def dispensary_list(request):
     dispensaries = Student.objects.all()
     context = {
         "title": "Dispensary List",
-        "dispensaries": "dispensaries"
+        "dispensaries": dispensaries
     }
     return render(request, "dispensary/dispensary_list.html", context=context)
+
+def per_student_view(request, pk):
+    students = get_object_or_404(Student, pk=pk)
+    context = {
+        'students': students
+    }
+    return render(request, "dispensary/per_student_view.html", context=context)
